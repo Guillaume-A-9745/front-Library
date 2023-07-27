febf94fa58db2200082b87866baef1aee070d128
Anselme Guillaume <anselmeguillaume@orange.fr>
2023-07-27 07:58:04 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BooksComponent } from './components/books/books.component';
+import { ComicsComponent } from './components/comics/comics.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    BooksComponent,
+    ComicsComponent
   ],
   imports: [
     BrowserModule,
